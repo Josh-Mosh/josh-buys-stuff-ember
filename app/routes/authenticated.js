18839987cbd466056209145488eadec7d7e5b0a3
Josh Gillespie <joshua.neal.gillespie@gmail.com>
2020-07-29 22:20:01 -0500

    add authenticated template

--- app/routes/authenticated.js
+++ app/routes/authenticated.js
@@ -1,7 +1,14 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
+  session: service(),
+
+  setupController(controller, /*model*/) {
+    controller.set('session', this.get('session'));
+  },
+
   actions: {
     invalidateSession() {
       this.get('session').invalidate();
